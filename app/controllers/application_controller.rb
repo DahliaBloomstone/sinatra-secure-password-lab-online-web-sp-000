bf6ee0c0c7f26ec4c98f5723005898ef1a466e8a
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 21:05:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+    	session[:user_id] = user.id
+    	redirect "/success"
+    else
+    	redirect "/failure"
+    end
+    end
   end
 
 #Renders failure.erb
